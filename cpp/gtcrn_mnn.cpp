728730f109e06326598d90ae77c82d7257a3b556
roycheng <919275390@qq.com>
2026-01-11 13:39:31 +0800

    Update gtcrn_mnn.cpp

--- cpp/gtcrn_mnn.cpp
+++ cpp/gtcrn_mnn.cpp
@@ -23,8 +23,6 @@ void GTCRNMNN::export_wav(
 
 void GTCRNMNN::inference(std::string input_audio_path, std::string output_audio_path) {
 
-	// 读取输入音频文件
-
 	AudioFile<float> input_file;
 	input_file.load(input_audio_path);
 	int audio_len = input_file.getNumSamplesPerChannel();
@@ -33,7 +31,7 @@ void GTCRNMNN::inference(std::string input_audio_path, std::string output_audio_
 		chunk_num += 1;
 	}
 
-	// 初始化输入 tensor
+	// init tensor
 	auto input_tensor = MNN::Express::_Input({ 257,2 }, MNN::Express::NCHW, halide_type_of<float>());
 	auto conv_cache_tensor = MNN::Express::_Input({ 1,16,16,66 }, MNN::Express::NCHW, halide_type_of<float>());
 	auto tra_cache_tensor = MNN::Express::_Input({ 6,1,1,16 }, MNN::Express::NCHW, halide_type_of<float>());
@@ -43,8 +41,6 @@ void GTCRNMNN::inference(std::string input_audio_path, std::string output_audio_
 	float* tra_cache_ptr = tra_cache_tensor->writeMap<float>();
 	float* inter_cache_ptr = inter_cache_tensor->writeMap<float>();
 
-
-	// 增强后的音频数据
 	std::vector<float> enhanced_data;
 
 	// FFT config
@@ -60,7 +56,6 @@ void GTCRNMNN::inference(std::string input_audio_path, std::string output_audio_
 	std::vector<cpx_type> fft_output(BLOCK_LEN);
 
 	for (int i = 0; i < chunk_num; i++) {
-		// 更新输入缓冲区
 		std::memmove(mic_buffer, mic_buffer + HOP_LEN, (BLOCK_LEN - HOP_LEN) * sizeof(float));
 
 		for (int n = 0; n < HOP_LEN; n++) {
@@ -72,25 +67,21 @@ void GTCRNMNN::inference(std::string input_audio_path, std::string output_audio_
 			}
 		}
 
-		// 加窗
 		for (int n = 0; n < BLOCK_LEN; n++) {
 			mic_in[n] = mic_buffer[n] * m_windows[n];
 		}
 
-		// FFT 取出实部和虚部
 		pocketfft::r2c(shape, stridel, strideo, axes, pocketfft::FORWARD, mic_in, fft_output.data(), 1.0);
 		for (int n = 0; n < FFT_OUT_SIZE; n++) {
 			spec[n * 2] = fft_output[n].real();
 			spec[n * 2 + 1] = fft_output[n].imag();
 		}
 
-		// 1. 输入数据准备
 		std::memcpy(input_ptr, spec, 257 * 2 * sizeof(float));
 		std::memcpy(conv_cache_ptr, conv_cache, 16 * 16 * 66 * sizeof(float));
 		std::memcpy(tra_cache_ptr, tra_cache, 6 * 16 * sizeof(float));
 		std::memcpy(inter_cache_ptr, inter_cache, 2 * 33 * 16 * sizeof(float));
 
-		// 2. 执行推理
 		std::vector<MNN::Express::VARP> outputs;
 		auto start = std::chrono::steady_clock::now();
 		outputs = module->onForward({ input_tensor, conv_cache_tensor, tra_cache_tensor, inter_cache_tensor });
@@ -98,8 +89,6 @@ void GTCRNMNN::inference(std::string input_audio_path, std::string output_audio_
 		std::chrono::duration<double, std::milli> ms = end - start;
 		std::cout << "耗时 " << ms.count() << " ms" << ", RTF: " << ms.count() / 1000 / 0.016 << std::endl;
 
-
-		// 3. 获取输出，并更新 cache
 		float y[FFT_OUT_SIZE * 2] = { 0 };
 		auto enh = outputs[0]->readMap<float>();
 		std::memcpy(y, enh, FFT_OUT_SIZE * 2 * sizeof(float));
@@ -110,7 +99,6 @@ void GTCRNMNN::inference(std::string input_audio_path, std::string output_audio_
 		auto inter_cache_out = outputs[3]->readMap<float>();
 		std::memcpy(inter_cache, inter_cache_out, 2 * 33 * 16 * sizeof(float));
 
-		// 更新输出 cache
 		for (int n = 0; n < FFT_OUT_SIZE; n++) {
 			enhanced_spec[n] = cpx_type(y[2 * n], y[2 * n + 1]);
 		}
